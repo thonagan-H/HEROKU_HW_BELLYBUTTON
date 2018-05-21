61b0bb7a65cb3910682f9bb95120dd0a80a9266d
thonagan-H <thonagan@yahoo.com>
2018-05-20 22:30:15 -0500

    Delete index.js

--- templates/index.js
+++
@@ -1,9 +0,0 @@
-var dropdown = document.getElementById("selDataset");
-var array = [1,2,3,4,5];
-
-for(var i = 0; i<arr.length;i++){
-    var elements = document.createElement("option")
-    elements.textContent = array[i]
-    elements.value = array[i]
-    dropdown.appendChild(elements);
-}
